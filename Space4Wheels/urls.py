f408228d4897269c32b33689eddb70d8d91d094a
larajako <lararjakosalem@gmail.com>
2023-12-15 09:01:59 +0800

    add users
    have profile page, login, logout, register

--- Space4Wheels/urls.py
+++ Space4Wheels/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('', views.home, name='Space4Wheels-home'),
     path('search/', views.search, name='Space4Wheels-search'),
     path('about/', views.about, name='Space4Wheels-about'),
+    path('register/', user_views.register, name='register'),
 ]
