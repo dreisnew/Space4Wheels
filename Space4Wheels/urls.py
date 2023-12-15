14b21036a0ece25954df5ca56d123bc6634e2bd1
larajako <lararjakosalem@gmail.com>
2023-12-15 16:19:56 +0800

    Added create, delete, update post but no buttons yet
    also updated the tabs

--- Space4Wheels/urls.py
+++ Space4Wheels/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 from . import views
-from users import views as user_views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView
+)
+#from users import views as user_views
 
 urlpatterns = [
-    path('', views.home, name='Space4Wheels-home'),
+    path('', PostListView.as_view(), name='Space4Wheels-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('search/', views.search, name='Space4Wheels-search'),
+    path('host/', views.host, name='Space4Wheels-host'),
+    path('bookings/', views.bookings, name='Space4Wheels-bookings'),
     path('about/', views.about, name='Space4Wheels-about'),
-    path('register/', user_views.register, name='register'),
+    #path('register/', user_views.register, name='register'),
 ]
--- Space4Wheels/views.py
+++ Space4Wheels/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
-posts = [
-    {
-        'author': 'Dre',
-        'title': 'Post 1',
-        'content': 'post 1 conetnt',
-        'date_posted': 'December 13, 2023'
-    },
-    {
-        'author': 'Lara',
-        'title': 'Post 2',
-        'content': 'post 2 conetnt',
-        'date_posted': 'December 14, 2023'
-    }
-]
+
 
 def home(request):
     context = {
@@ -23,8 +17,56 @@ def home(request):
     }
     return render(request, 'Space4Wheels/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'Space4Wheels/home.html' # app>/<model>_<viewtype.html>
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content'] # add the fields you want for the user to add/edit
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content'] # add the fields you want for the user to add/edit
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 def search(request):
     return render(request, 'Space4Wheels/search.html', {'title': 'Search'})
+
+def host(request):
+    return render(request, 'Space4Wheels/host.html', {'title': 'Host'})
+
+def bookings(request):
+    return render(request, 'Space4Wheels/bookings.html', {'title': 'Bookings'})
 
 def about(request):
     return render(request, 'Space4Wheels/about.html', {'title': 'About'})
