9ba0e63d3c06b1bc721c11c01755a5118d61697a
larajako <lararjakosalem@gmail.com>
2023-12-18 16:33:24 +0800

    updated few things
    1. map display iframe
    [search.html]
    - added the iframe
    - removed the div width containing iframe to be 100% since that goes out of the box
    
    2. search using country or city
    [space4wheels/views.py]
    - simply add the 'or' operator
    - Post.objects.filter(title__icontains=query) | Post.objects.filter(city__icontains=query) | Post.objects.filter(country__icontains=query)
    
    3. removed sidenav
    [base.html]
    - remove div containing sidenav
    - adjust css class for main tag containing all the posts
    <main role="main" class="container-fluid">
          <div class="row">
            <div class="container">
    
    4. update models.py to make status accept only available/unavailable
    STATUS_TYPES = [
            ('available', 'Available'),
            ('unavailable', 'Unavailable')
        ]
    status = models.CharField(max_length=20, default='available', choices=STATUS_TYPES)  # available/unavailable
    
    5. added absolute urls to Update, Delete, and Create function in Space4Wheels/views.py

--- Space4Wheels/admin.py
+++ Space4Wheels/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Booking
 
 # Register your models here.
 admin.site.register(Post) #post model
+admin.site.register(Booking) #post model
