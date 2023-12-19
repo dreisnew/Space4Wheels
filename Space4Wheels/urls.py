8219359624f7913c94bd01a598ded20e6495cb53
larajako <lararjakosalem@gmail.com>
2023-12-19 12:47:35 +0800

    added login required decorator to some urls

--- Space4Wheels/urls.py
+++ Space4Wheels/urls.py
@@ -9,15 +9,16 @@ from .views import (
     book_space, BookingsView, rate_post
 )
 #from users import views as user_views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', PostListView.as_view(), name='Space4Wheels-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', login_required(PostDetailView.as_view()), name='post-detail'),
+    path('post/new/', login_required(PostCreateView.as_view()), name='post-create'),
+    path('post/<int:pk>/update/', login_required(PostUpdateView.as_view()), name='post-update'),
+    path('post/<int:pk>/delete/', login_required(PostDeleteView.as_view()), name='post-delete'),
     path('search/', views.search, name='Space4Wheels-search'),
-    path('host/', views.host, name='Space4Wheels-host'),
+    path('host/', login_required(views.host), name='Space4Wheels-host'),
     path('about/', views.about, name='Space4Wheels-about'),
     path('bookings/', BookingsView.as_view(), name='bookings'),
     path('post/<int:post_id>/book_space/', book_space, name='book-space'),
